deed7f10900666767316804c9ed61812498fd24d
chxrbelm <charbelmoubarakk@outlook.com>
2025-10-10 10:27:31 +1100

    Update paymentRoutes.js

--- backend/apps/routes/paymentRoutes.js
+++ backend/apps/routes/paymentRoutes.js
@@ -1,12 +1,12 @@
 // apps/routes/paymentRoutes.js
 const express = require('express');
 const router = express.Router();
-const { createCheckoutSession, confirmPayment } = require('../controllers/paymentController');
+const { createCheckoutSession, confirmPayment, getPaymentStatus } = require('../controllers/paymentController');
 
 // POST /api/payments/create-checkout-session
 router.post('/create-checkout-session', createCheckoutSession);
 
 // POST /api/payments/confirm
 router.post('/confirm', confirmPayment);
-
+router.get('/status', getPaymentStatus);
 module.exports = router;
