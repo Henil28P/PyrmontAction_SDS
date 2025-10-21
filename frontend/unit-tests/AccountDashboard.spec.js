b772660fd52421dc7b07be793ab11c0b68acc4ba
Henil <henilp467@gmail.com>
2025-10-21 20:18:44 +1100

    set up unit tests for Account Dashboard feature

---
+++ frontend/unit-tests/AccountDashboard.spec.js
@@ -0,0 +1,17 @@
+// import { mount } from '@vue/test-utils'
+// import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { mount } from "@vue/test-utils";
+import EditAccountDetailsModal from "../../frontend/src/features/accountDashboard/components/EditAccountDetailsModal.vue";
+import services from '@/features/accountDashboard/dashboardServices'
+import { createPinia } from 'pinia';
+
+vi.mock("../../frontend/src/features/accountDashboard/dashboardServices", () => ({
+  default: {
+    updateCurrentUser: vi.fn().mockResolvedValue({ email: 'updated@example.com' })
+  },
+}));
+
+describe('EditAccountDetailsModal.vue', () => {
+    const mockUser = { email: 'Amy.Green@example.com' }
+})
