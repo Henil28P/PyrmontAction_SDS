4c1b81c572f17421ba37e992c2ac537ca47b094d
Henil <henilp467@gmail.com>
2025-10-23 08:34:33 +1100

    [unit-tests/EditorialDashboard.spec.js]: set up mock for Editorial Dashboard unit tests

--- frontend/unit-tests/EditorialDashboard.spec.js
+++ frontend/unit-tests/EditorialDashboard.spec.js
@@ -1,6 +1,15 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
 
+vi.mock("@/features/editorialDashboard/editorialServices", () => ({
+  default: {
+    createEvent: vi.fn(),
+    updateEvent: vi.fn(),
+    publishEvent: vi.fn(),
+    deleteEvent: vi.fn(),
+  },
+}));
+
 describe("Editorial Dashboard - Events Unit Tests", () => {
   beforeEach(() => {
     vi.clearAllMocks();
