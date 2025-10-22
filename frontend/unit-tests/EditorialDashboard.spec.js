74a2f3278d8bae530324dec60c64bd3d639d9963
Henil <henilp467@gmail.com>
2025-10-23 08:32:08 +1100

    create new unit test file for Editorial Dashboard new feature

---
+++ frontend/unit-tests/EditorialDashboard.spec.js
@@ -0,0 +1,8 @@
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { mount } from "@vue/test-utils";
+
+describe("Editorial Dashboard - Events Unit Tests", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+});
