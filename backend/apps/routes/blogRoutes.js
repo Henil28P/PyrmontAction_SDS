c291e06b8510776f18edbb3bc57a75d7ce2c2bd3
Anthony He <anthonyhe3154@gmail.com>
2025-10-31 22:53:37 +1100

    added JWT and role verification for some sensitive blog routes to authorise for Admin or Editor.

--- backend/apps/routes/blogRoutes.js
+++ backend/apps/routes/blogRoutes.js
@@ -3,18 +3,41 @@ const controller = require('../controllers/blogController');
 const jwtAuth = require('../middlewares/jwtMiddleware');
 
 /* CREATE */
+// Visitor can submit a blog post
 router.post('/', controller.submitBlog);
 
 /* READ */
-router.get('/', controller.getAllBlogs);
+// Admin: List all blogs
+router.get('/', 
+    jwtAuth.verifyToken,
+    jwtAuth.verifyRole(['admin', 'editor']),
+    controller.getAllBlogs
+);
+// Public: List approved blogs
 router.get('/approved', controller.getApprovedBlogs);
+// Get blog by ID
 router.get('/:id', controller.getBlogByID);
 
 /* UPDATE */
-router.put('/:id', controller.updateBlog);
-router.put('/:id/approve', controller.approveBlog);
+// Admin: Approve a blog post
+router.put('/:id/approve', 
+    jwtAuth.verifyToken,
+    jwtAuth.verifyRole(['admin', 'editor']),
+    controller.approveBlog
+);
+// Admin: Update a blog post
+router.put('/:id', 
+    jwtAuth.verifyToken,
+    jwtAuth.verifyRole(['admin', 'editor']),
+    controller.updateBlog
+);
 
 /* DELETE */
-router.delete('/:id', controller.deleteBlog);
+// Admin: Delete a blog post
+router.delete('/:id', 
+    jwtAuth.verifyToken,
+    jwtAuth.verifyRole(['admin', 'editor']),
+    controller.deleteBlog
+);
 
 module.exports = router;
